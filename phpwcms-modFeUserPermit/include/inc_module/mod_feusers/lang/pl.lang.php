eae605c0452d227e1f44d0d338800ed915973559
Chaffinch <bobi@poczta.fm>
2016-07-11 22:50:32 +0200

    Create pl.lang.php

---
+++ phpwcms-modFeUserPermit/include/inc_module/mod_feusers/lang/pl.lang.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * phpwcms content management system
+ *
+ * @author Oliver Georgi <og@phpwcms.org>
+ * @copyright Copyright (c) 2002-2015, Oliver Georgi
+ * @license http://opensource.org/licenses/GPL-2.0 GNU GPL-2
+ * @link http://www.phpwcms.de
+ *Language: Polish, Language Code: pl UTF-8 for ver.1.8.4 (2016/07/09, 543)
+//Zięba Bogusław http://www.krynica.malopolska.pl
+ **/
+
+// first define main language vars
+// first define main language vars
+// first define main language vars
+// first define main language vars
+$BLM['backend_menu']			= 'Uprawnienia Użytkownika Witryny';
+
+$BLM['listing_title']			= 'Zarządzanie uwierzytelnianiem użytkowników witryny';
+$BLM['create_new']			= 'Dodaj użytkownika';
+$BLM['florist_entry']			= 'Dane konta';
+$BLM['florist_title']			= 'Tytuł';
+$BLM['delete_entry']			= 'Usuń konto:';
+$BLM['listview']			= 'Widok listy';
+$BLM['max_words']			= 'maks. słów';
+$BLM['no_entry']			= 'Nie znaleziono pozycji';
+
+$BLM['detail_title']			= 'Pozdrawiamy';
+$BLM['detail_firstname']		= 'Imię';
+$BLM['detail_lastname']			= 'Nazwisko';
+$BLM['detail_company']			= 'Firma';
+$BLM['detail_street']			= 'Ulica';
+$BLM['detail_city']			= 'Miasto';
+$BLM['detail_zip']			= 'Kod pocztowy';
+$BLM['detail_region']			= 'Województwo';
+$BLM['detail_country']			= 'Kraj';
+$BLM['detail_fon']			= 'Telefon';
+$BLM['detail_fax']			= 'Fax';
+$BLM['detail_mobile']			= 'Komórkowy';
+$BLM['detail_signature']		= 'Podpis';
+$BLM['detail_aktiv']			= 'Aktywny';
+$BLM['detail_newsletter']		= 'Biuletyn';
+$BLM['detail_website']			= 'Strona interentowa';
+$BLM['detail_int1']			= 'Przypisany poziom struktury';
+$BLM['detail_varchar1']			= 'ID Klienta';
+$BLM['detail_email']			= 'E-mail';
+$BLM['detail_login']			= 'Login';
+$BLM['detail_password']			= 'Hasło';
+
+$BLM['forminfo']	= 'Hasło musi mieć co najmniej 5 znaków. Nie wpisuj dowolnego ciągu jak długo to nie powinno być aktualizowane.';
+
+$BLM['error_password']	= 'Pole hasło jest obowiązkowe.';
+$BLM['error_password_short'] = 'Hasło musi mieć co najmniej 5 znaków.';
+$BLM['error_login']	= 'Login nie jest unikalny. Wybierz inny login.';
+$BLM['error_email']	= 'Prawidłowy adres e-mail jest obowiązkowy.';
+$BLM['error_int1']	= 'Wybierz dowolny poziom serwisu różniący się od poziomu głównego.';
+?>
